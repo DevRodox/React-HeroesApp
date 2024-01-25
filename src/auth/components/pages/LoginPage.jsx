ca51a696915079c27bac63599bf08e6be1f53b81
Rodox <rodrigo17.ed@gmail.com>
2024-01-25 17:00:21 -0600

    feat: add private and public route to AppRouter

--- src/auth/components/pages/LoginPage.jsx
+++ src/auth/components/pages/LoginPage.jsx
@@ -11,7 +11,7 @@ export const LoginPage = () => {
 
     login('rodox');
 
-    navigate('/',{
+    navigate('/marvel',{
       replace: true
     });
   };
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,17 +2,23 @@ import { Navigate, RouterProvider, createBrowserRouter } from "react-router-dom"
 
 import { LoginPage } from '../auth';
 import { HeroesMain, childHeroesRouter } from '../heroes'
+import { PrivateRoute } from "../heroes/components/routes/PrivateRoute";
+import { PublicRoute } from "../heroes/components/routes/PublicRoute";
 
 const router = createBrowserRouter ([
     {
         path: "/*",
-        element: <HeroesMain />,
+        element: <PrivateRoute>
+                    <HeroesMain />
+                 </PrivateRoute> ,
         errorElement: <Navigate to="/"/>,
         children: childHeroesRouter 
     }, 
     {
         path: "/login",
-        element: <LoginPage />
+        element: <PublicRoute>
+                    <LoginPage />
+                 </PublicRoute>
     }
 ]); 
 
