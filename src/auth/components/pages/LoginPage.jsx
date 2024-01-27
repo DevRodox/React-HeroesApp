6c8bfb555f3b0507c25a133ec963fb4e9c338fff
Rodox <rodrigo17.ed@gmail.com>
2024-01-26 23:42:58 -0600

    feat: add lastPath to loginPage

--- src/auth/components/pages/LoginPage.jsx
+++ src/auth/components/pages/LoginPage.jsx
@@ -9,9 +9,11 @@ export const LoginPage = () => {
 
   const onLogin = () => {
 
+    const lastPath = localStorage.getItem('lastPath') || '/';
+
     login('rodox');
 
-    navigate('/marvel',{
+    navigate(lastPath, {
       replace: true
     });
   };
