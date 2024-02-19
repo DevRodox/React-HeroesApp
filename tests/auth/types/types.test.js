a26d3184eff123860f26d95d1c29eab325c1c834
DevRodox <rodrigo17.ed@gmail.com>
2024-02-19 12:51:20 -0600

    Add types tests

---
+++ tests/auth/types/types.test.js
@@ -0,0 +1,12 @@
+import { types } from "../../../src/auth"
+
+describe('Pruebas en types', () => {
+
+    test('debe de regresar estos types', () => {
+        expect( types ).toEqual( {
+            login:  '[Auth] Login',
+            logout: '[Auth] Logout',
+        });
+    });
+});
+
